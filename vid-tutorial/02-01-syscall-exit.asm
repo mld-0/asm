9c8f354bfc9b04cf3c19e50f976e80267525741d
Matthew L Davis <mld.0@protonmail.com>
2022-06-20 08:04:47 +1000

    end state infinite loop
    Infinite loop serves as end state because CPUlator does not support exit
    syscall(?)

--- vid-tutorial/02-01-syscall-exit.asm
+++ vid-tutorial/02-01-syscall-exit.asm
@@ -1,7 +1,7 @@
 
 #	Instructions:
-#
-#	mov{S}{cond} Rd, Operand2		(move)
+#	move:
+#	mov{S}{cond} Rd, Operand2
 #	mov{cond} Rd, #imm16
 #		{S}:		(optional) suffix
 #		{cond}:		(optional) conditional code
@@ -9,7 +9,8 @@
 #		Operand2:	flexible second operand
 #		imm16:		value in range 0-65535 
 #
-#	swi immed_8						(software interupt)
+#	software interrupt (uses r7)
+#	swi immed_8
 #		immed_8		value in range 0-255 (used by exception handler)
 
 #	export labels
@@ -18,12 +19,24 @@
 #	start label
 _start:
 
-	#	place value in r0
+	#	move 5 -> r0
+	mov r0, #5
+	#	move r0 -> r1
+	mov r1, r0
+	#	move 30 -> r0
 	mov r0, #30
 
-	#	r7 is used for syscalls, 1 is the code to exit
-	mov r7, #1
 
-	#	syscall, r7=1 -> exit
-	swi 0
+	#	exit syscall (not supported on CPULator)
+	##	r7 is used for syscalls, 1 is the code to exit
+	#mov r7, #10
+	##	syscall, r7=1 -> exit
+	#swi 0
+
+
+#	end state (CPULator does not support exit syscall(?), infinite loop serves as our end state.
+S:
+	b S
+
+.end
 
