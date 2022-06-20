666d5dcbd35daf930599ba26171b2defcc3aeed5
Matthew L Davis <mld.0@protonmail.com>
2022-06-20 23:46:49 +1000

    separate functions for heap/stack arrays

--- src/heap-vs-stack-array.cpp
+++ src/heap-vs-stack-array.cpp
@@ -1,12 +1,15 @@
 
-void run_test(const int SIZE) 
+void example_heap(const int SIZE) 
 {
 	int v1[SIZE];
 	v1[0] = 53;
 	for (int i = 1; i < SIZE; ++i) {
 	    v1[i] = v1[i - 1];
 	}
+}
 
+void example_stack(const int SIZE) 
+{
 	int *v2 = new int[SIZE]; 
 	v2[0] = 21;
 	for (int i = 1; i < SIZE; ++i) {
@@ -17,8 +20,8 @@ void run_test(const int SIZE)
 
 int main()
 {
-	run_test(2'000'000);
+	example_heap(2'000'000);
+	example_stack(2'000'000);
 	return 0;
 }
-
 
