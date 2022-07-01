7897d07ae198adce95e5d1f3e8f5926b9b2d79c2
Matthew L Davis <mld.0@protonmail.com>
2022-07-01 14:55:06 +1000

    replace links with file copies

--- apress-programming-arm/00-hello-world/hello-world.s
+++ apress-programming-arm/00-hello-world/hello-world.s
@@ -1,1 +1,80 @@
-../../Minerva/hello-world.s
+#   vim: set tabstop=4 modeline modelines=10:
+#   vim: set foldlevel=2 foldcolumn=2 foldmethod=marker:
+//	{{{2
+//	Ongoings:
+//	{{{
+//	Ongoing: 2022-06-24T02:28:23AEST need to use 'adr' (not 'ldr') (because macOS linker/loader doesn't like doing relocations)
+//	Ongoing: 2022-06-24T02:30:35AEST Linux uses '_start', macOS uses '_main'(?)
+//	Ongoing: 2022-06-24T01:41:09AEST no '.data' section? [...] (create a data section, as you think it would look, and it works, as you think it would work?)
+//	}}}
+
+//	LINK: https://smist08.wordpress.com/2021/01/08/apple-m1-assembly-language-hello-world/
+
+//	ARM64 Registers:
+//	{{{
+//	General Purpouse (64-bit) (use 'W' for 32-bit registers)
+//		X0-X7		parameters/results
+//		X8			indirect result location
+//		X9-X15		temporary 
+//		X16			(IP0) intra-procedure-call temp (syscalls)
+//		X17			(IP1) intra-procedure-call temp (syscalls?)
+//		X18			(reserved) platform register
+//		X19-X28		callee-saved regsiters
+//		X29			(FP) frame pointer
+//		X30			(LR) link register
+//	Special
+//		SP			stack pointer
+//		PC			program counter (address of current instruction)
+//		NZCV		global condition flag 
+//		FPSR		floating point status
+//		FPCR		floating point control
+//		XZR			zero
+//	Floating-point/SIMD
+//		D0-D7		argument/result values
+//		D8-D15		preserved across calls
+//		D16-D31		temporary
+//	}}}
+
+//	Move:				
+//			mov{S}{cond} Rd, Operand2
+//			mov{cond} Rd, #imm16
+//		
+//	Register-Relative: 	
+//	Generate a PC-relative address in 'Rd' for 'label' (must be within limited distance of current instruction).
+//			adr{cond}{.W} Rd, label
+//
+//	Supervisor (syscall)
+//	Causes an exception (switch processor to supervisor mode)
+//			svc{cond} #imm
+
+
+//	Default entry-point is '_main' for macOS, '_start' for Linux
+.global _start
+
+//	Must start on 64-bit boundary
+.align 2
+
+_start:
+	//	1 = stdout
+	mov x0, #1
+
+	//	output string
+	//	<(can't use ldr?)>
+	adr x1, helloworld
+	//ldr x1, =helloworld
+
+	//	12 = length of string
+	mov x2, #12
+
+	//	macOS 'write' syscall
+	mov x16, #4
+	svc 0
+
+	//	macOS 'exit' syscall
+	mov x0, #0
+	mov x16, #1
+	svc 0
+
+.data:
+	helloworld: .ascii "Hello World\n"
+
--- apress-programming-arm/00-hello-world/run-hello-world.sh
+++ apress-programming-arm/00-hello-world/run-hello-world.sh
@@ -1,1 +1,15 @@
-../../Minerva/run-hello-world.sh
+
+[ -f "hello-world.o" ] && rm "hello-world.o"
+[ -f "hello-world" ] && rm "hello-world"
+
+#	TODO: 2022-06-24T01:51:54AEST asm/Minerva/run-hello-world, clang, assembly, source -> executable in one command (without creating .o file)
+
+clang -o "hello-world.o" -c "hello-world.s" 
+clang -o "hello-world" "hello-world.o" -e _start
+
+#	or: (produces identical binary)
+#as "hello-world.s" -o "hello-world.o" 
+#ld "hello-world.o" -lSystem -syslibroot `xcrun -sdk macosx --show-sdk-path` -e _start -arch arm64 -o "hello-world"
+
+./hello-world
+
