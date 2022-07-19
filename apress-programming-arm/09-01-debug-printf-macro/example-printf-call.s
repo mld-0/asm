ca492837e28ff40c1f5694486d012ce703038988
Matthew L Davis <mld.0@protonmail.com>
2022-07-19 23:35:56 +1000

    update stack usage, add 'nl' example
    Correct indexing relative to both SP and FP.

--- apress-programming-arm/09-01-debug-printf-macro/example-printf-call.s
+++ apress-programming-arm/09-01-debug-printf-macro/example-printf-call.s
@@ -2,42 +2,54 @@
 //	vim: set tabstop=4 modeline modelines=10:
 //	vim: set foldlevel=2 foldcolumn=2 foldmethod=marker:
 //	{{{2
+//	Ongoings:
+//	{{{
+//	Ongoing: 2022-07-19T22:53:46AEST (cleaner) (more explicit/consistent) pushing/popping [...] (make clear how to pass variables on the stack of a variadic function) ... (printing a range of memory?) 
+//	Ongoing: 2022-07-19T22:54:54AEST what do we have to do to make '_printf()' available? [...] what other functions are available?
+//	}}}
 
-//	See 'HelloSilicon' example on calling _printf
+//	'_printf' takes the address of a string as x0. The remaining arguments are variatic. 
+//	On Apple Silicon (Darwin), variatic arguments are passed, 8-byte-aligned, on the stack.
 
 .text
-.global _start
-
 .align 2
-
+.global _start
 _start:
+
+printf_teststr:
 	stp FP, LR, [SP, #-16]!			//	push FP, LR
 
-	adrp 	x0, str_printf@PAGE		//	x0 = &str_printf
-	add x0, x0, str_printf@PAGEOFF
-
-	mov x2, #4711
+	adrp 	x0, teststr@PAGE		//	x0 = &teststr
+	add x0, x0, teststr@PAGEOFF
+	mov x10, #65					//	65 = 'a'
+	mov x2, #1234
 	mov x3, #2845
-	mov x10, #65
 
-	//	Ongoing: 2022-07-19T22:53:46AEST (cleaner) (more explicit/consistent) pushing/popping [...] (make clear how to pass variables on the stack of a variadic function) ... (printing a range of memory?) 
-	str x10, [SP, #-32]!
-	str x2, [SP, #8]
-	str x3, [SP, #16]
+	mov FP, SP						//	save arguments on stack
+	sub SP, SP, #32
+	str x10, [SP, #0]				//	[FP, #-32]
+	str x2,  [SP, #8]				//	[FP, #-24]
+	str x3,  [SP, #16]				//	[FP, #-16]
 
-	//	Ongoing: 2022-07-19T22:54:54AEST what do we have to do 
-	bl _printf
+	bl 	_printf
 
-	add SP, SP, #32					
+	mov SP, FP						//	restore stack
 
 	ldp FP, LR, [SP], #16			//	pop FP, LR
 	
 
+printf_nl:
+	adrp 	x0, charcode@PAGE
+	add x0, x0, charcode@PAGEOFF
+	mov x1, #0x0a					//	0x0a = '\n'
+	mov FP, SP
+	sub SP, SP, #-16
+	str x1, [SP]
+	bl 	_printf
+	mov SP, FP
 
-	//	Use _printf to print a single character (newline)
-	//	<>
 
-
+exit_done:
 	//	Write 'donemsg'
 	mov x0, #1						//	1 = stdout
 	adr x1, donemsg
@@ -55,5 +67,7 @@ _start:
 	.align 4
 	donemsg: .ascii "Done\n"
 	.align 4
-	str_printf: .asciz "Hello (printf) World, c=(%c), ld=(%ld), ld=(%ld)\n"
+	teststr: .asciz "Hello (printf) World, c=(%c), ld=(%ld), ld=(%ld)\n"
+	.align 4
+	charcode: .asciz "(%c)\n"
 
