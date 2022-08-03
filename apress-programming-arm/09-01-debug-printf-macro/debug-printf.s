6345efa2276c660993dc1a6b2f70e13bc92ac4a1
Matthew L Davis <mld.0@protonmail.com>
2022-08-03 22:56:45 +1000

    debug printf macro - print-memory bytes

--- apress-programming-arm/09-01-debug-printf-macro/debug-printf.s
+++ apress-programming-arm/09-01-debug-printf-macro/debug-printf.s
@@ -6,11 +6,45 @@
 //	{{{
 //	Ongoing: 2022-07-20T00:21:10AEST '.text' '.align 2' ect. for macros source
 //	Ongoing: 2022-08-03T18:54:16AEST Using '.data' string variables vs strings embedded at a label in macro?
+//	Ongoing: 2022-08-03T22:22:11AEST handle labeling of registers containing multiple digits
 //	}}}
 
 //	Continue: 2022-08-03T17:26:57AEST add label string argument to print register macros
 
 //	These macros preseve all registers. Beware they will change the condition flags.
+
+//	'printf_reg(reg)':
+//		reg:		must be a register number (as literal integer, '3' is valid but '#3' is not)
+.macro 		printf_reg		reg
+	push_registers
+	mov x2, x\reg 					//	%lx, %ld
+	mov x1, #\reg + '0'				//	%c
+	mov FP, SP
+	sub SP, SP, #32
+	str x1, [SP, #0]
+	str x2, [SP, #8]
+	str x2, [SP, #16]
+	adrp 	x0, str_printf_reg@PAGE
+	add x0, x0, str_printf_reg@PAGEOFF
+	bl	_printf
+	mov SP, FP
+	pop_registers
+.endm
+
+
+//	'printf_str(str)':
+//		str: 		must be a literal string
+.macro 		printf_str		str
+	push_registers
+	adr x0, 1f
+	bl 	_printf
+	pop_registers
+	b 	2f
+1:	.asciz "\str\n"
+	.align 4
+2: 	
+.endm
+
 
 //	'printf_[float|double]_reg(reg)'
 //		reg:		must be a register number (as literal integer, '3' is valid but '#3' is not)
@@ -48,13 +82,21 @@
 .endm
 
 
-//	printf_memory_ints(start,end): print a memory range as integers
+//	printf_memory_[ints|bytes](start,end): print a memory range as ints/bytes
 //		start:		register containing start of memory range
 //		len:		number of bytes (as literal integer)
 .macro 		printf_memory_ints 		start, len
 	push_registers
 	mov x19, \start
 	mov x20, #\len
+	adrp 	x0, str_printf_report_ints@PAGE
+	add x0, x0, str_printf_report_ints@PAGEOFF
+	mov FP, SP
+	sub SP, SP, #16
+	str x19, [SP, #0]
+	str x20, [SP, #8]
+	bl 	_printf
+	mov SP, FP
 	adrp 	x21, str_printf_int@PAGE
 	add x21, x21, str_printf_int@PAGEOFF
 1: 	ldr w3, [x19]
@@ -72,41 +114,34 @@
 	bl 	_printf
 	pop_registers
 .endm
-
 .macro 		printf_memory_bytes		start, len
-.endm
-
-
-//	'printf_reg(reg)':
-//		reg:		must be a register number (as literal integer, '3' is valid but '#3' is not)
-.macro 		printf_reg		reg
 	push_registers
-	mov x2, x\reg 					//	%lx, %ld
-	mov x1, #\reg + '0'				//	%c
+	mov x19, \start
+	mov x20, #\len
+	adrp 	x0, str_printf_report_bytes@PAGE
+	add x0, x0, str_printf_report_bytes@PAGEOFF
 	mov FP, SP
-	sub SP, SP, #32
-	str x1, [SP, #0]
-	str x2, [SP, #8]
-	str x2, [SP, #16]
-	adrp 	x0, str_printf_reg@PAGE
-	add x0, x0, str_printf_reg@PAGEOFF
-	bl	_printf
+	sub SP, SP, #16
+	str x19, [SP, #0]
+	str x20, [SP, #8]
+	bl 	_printf
 	mov SP, FP
-	pop_registers
-.endm
-
-
-//	'printf_str(str)':
-//		str: 		must be a literal string
-.macro 		printf_str		str
-	push_registers
-	adr x0, 1f
+	adrp 	x21, str_printf_byte@PAGE
+	add x21, x21, str_printf_byte@PAGEOFF
+1:	ldrb w3, [x19]
+	mov FP, SP
+	sub SP, SP, #16
+	mov x0, x21
+	str w3, [SP, #0]
+	bl 	_printf
+	mov SP, FP
+	add x19, x19, #1
+	subs x20, x20, #1
+	b.gt 	1b
+	adrp x0, str_printf_nl@PAGE
+	add x0, x0, str_printf_nl@PAGEOFF
 	bl 	_printf
 	pop_registers
-	b 	2f
-1:	.asciz "\str\n"
-	.align 4
-2: 	
 .endm
 
 
@@ -145,7 +180,13 @@
 .endm
 
 .data
+	str_printf_report_bytes:	.asciz 		"bytes, addr=(0x%016lX), len=(%d):\n"
+	.align 4
+	str_printf_report_ints:		.asciz 		"ints, addr=(0x%016lX), len=(%d):\n"
+	.align 4
 	str_printf_int: .asciz "%i "
+	.align 4
+	str_printf_byte: 	.asciz 	"%02X "
 	.align 4
 	str_printf_nl: 	.asciz 	"\n"
 	.align 4
